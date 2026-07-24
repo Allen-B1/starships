0c50e816ad23d780cc06c716d9442e7d9fee7f74
Allen B <pingguo.ny@gmail.com>
2026-07-24 18:20:25 -0400

    stuff

---
+++ fortresses.js
@@ -0,0 +1,31 @@
+import fs from 'node:fs';
+import os from 'node:os';
+import YAML from 'yaml';
+import { exec } from 'child_process';
+import { wrap, createCard, createDeck } from './tts.js';
+import { execSync } from 'node:child_process';
+
+const yaml = fs.readFileSync('./fortresses.yml', 'utf8');
+const fortresses = YAML.parse(yaml);
+const template = fs.readFileSync("./fortress-template.svg", 'utf8');
+
+let cards = [];
+for (let fortressName in fortresses) {
+    let description = fortresses[fortressName];
+
+    if (description.split(":").length > 0) {
+        description = `<tspan fill="#d4ccff">` +  description.split(":")[0] + ":</tspan>" + description.split(":").slice(1).join(":");
+    }
+
+    const svg = template.replace("Ability Text", "<tspan x=\"0\">" + description.replaceAll("\n", "</tspan><tspan x=\"0\" dy=\"1.2em\">") + "</tspan>")
+        .replace("Name of Fortress", fortressName);
+    const id = fortressName.toLowerCase().replace(/\s+/g, "-");
+    fs.writeFileSync("out/fortress-" + id + ".svg", svg);
+    execSync(`convert out/fortress-${id}.svg png/fortress-${id}.png`);
+    cards.push([`https://raw.githubusercontent.com/allen-b1/starships/refs/heads/master/png/fortress-${id}.png`]);
+}
+
+execSync(`convert fortress-back.svg png/fortress-back.png`);
+fs.writeFileSync(os.homedir() + "/.local/share/Tabletop Simulator/Saves/Saved Objects/starships/fortress-deck.json", 
+    JSON.stringify(wrap(createDeck("https://raw.githubusercontent.com/allen-b1/starships/refs/heads/master/png/fortress-back.png", cards)), null, "\t")
+);
